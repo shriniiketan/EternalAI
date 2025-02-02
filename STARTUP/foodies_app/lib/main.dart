c676541d2cd45dd726432fb1f9301ec69a91237d
shriniiketan <59924416+shriniiketan@users.noreply.github.com>
2025-02-02 15:45:49 -0800

    added startup page

--- STARTUP/foodies_app/lib/main.dart
+++ STARTUP/foodies_app/lib/main.dart
@@ -1,9 +1,22 @@
 import 'package:flutter/material.dart';
 void main(){
   runApp(
-     Center(
-       child: Text(
-         'Hello World', textDirection: TextDirection.ltr,),
-     )
+    MaterialApp(
+      title: 'Foodies App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Foodiez'.toUpperCase(),
+              style: TextStyle(fontFamily: "Oswald" , fontSize: 35.0, color: Colors.tealAccent),
+        ),
+          backgroundColor: Colors.purpleAccent,
+        ),
+        body: Center(
+          child: Text(
+              "Hello World",
+              textDirection: TextDirection.ltr, )
+        ),
+      )
+    )
   );
 }
